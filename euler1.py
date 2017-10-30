10bdd61eadc3613d633700b741bf83aeb038cb4b
Simon Kingsley <simon.kingsley@oxinst.com>
2017-10-30 14:20:58 +0000

    First attempt - gets a result.
    Is it right though?

--- euler1.py
+++ euler1.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python3
+from asyncio.streams import LimitOverrunError
+
+
 
 def main():
-    print("hello world")
-    print("boo")
-    pass
+    result3 = 0
+    result5 = 0
+    result15 = 0
+    
+    i = 1
+    limit = 1000
+    total = 0
+    
+    while result3 <= limit:
+        result3 = i * 3
+        total = total + result3
+        i = i + 1
+    
+    i = 1    
+    while result5 <= limit:
+        result5 = i * 5
+        total = total + result5
+        i = i + 1
+        
+    i = 1
+    while result15 <= limit:
+        result15 = i * 15
+        total = total - result15
+        i = i + 1
+        
+    print(total)
+
 
 if __name__ == "__main__":
     main()
