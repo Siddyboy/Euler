f62570c6fb825ebb484a1484df160816c07e0ef8
Simon Kingsley <simon.kingsley@oxinst.com>
2017-10-31 16:32:06 +0000

    Add nice solution after a bit of thought.
    Solution from euler archive.

--- euler1.py
+++ euler1.py
@@ -1,37 +1,10 @@
 #!/usr/bin/env python3
-from asyncio.streams import LimitOverrunError
-
-
 
 def main():
-    result3 = 0
-    result5 = 0
-    result15 = 0
-    
-    i = 1
     limit = 1000
-    total = 0
-    
-    while result3 <= limit:
-        result3 = i * 3
-        total = total + result3
-        i = i + 1
-    
-    i = 1    
-    while result5 <= limit:
-        result5 = i * 5
-        total = total + result5
-        i = i + 1
-        
-    i = 1
-    while result15 <= limit:
-        result15 = i * 15
-        total = total - result15
-        i = i + 1
-        
-    print(total)
-
+              
+    a = sum(x for x in range(limit) if x % 3 == 0 or x % 5 == 0)
+    print(a)
 
 if __name__ == "__main__":
     main()
-
