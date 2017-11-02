0158e55338569a40dd772c2581b17bdd722f46e0
Simon <scjk@btinternet.com>
2017-11-02 23:19:39 +0000

    Okay
    Starting to code

--- euler2/euler2sk.py
+++ euler2/euler2sk.py
@@ -3,9 +3,13 @@
 def main():
     limit = 1000
               
-    a = sum(x for x in range(limit) if x % 3 == 0 or x % 5 == 0)
+    a = sum(x for x in range(limit) if x % 2 == 0 and fib(x))
     print(a)
     print(a)
+
+def fib(a):
+    
+    return
 
 if __name__ == "__main__":
     main()
