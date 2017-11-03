c7b81cee063e67c3e2ce7c4a13dbb99815876564
Simon Kingsley <simon.kingsley@oxinst.com>
2017-11-03 13:27:02 +0000

    Start to edit but not finished.
    Trying hard to use comprehensions

--- euler2/euler2sk.py
+++ euler2/euler2sk.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
+def isfib(a):
+    while fib <= a
+        If a == fib
+            
+    
+    pass
+
 def main():
     limit = 1000
               
-    a = sum(x for x in range(limit) if x % 2 == 0 and fib(x))
+    a = sum(x for x in range(limit) if x % 2 == 0 and if x in fib)
     print(a)
     print(a)
 
